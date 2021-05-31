208a5a4cb595d6f593ef2c17000e406e2dfcca0c
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2021-05-31 16:26:57 +0900

    update

---
+++ Navigation/MapServer.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+get_classpath()
+{
+  FILE1=`ls ${RTM_JAVA_ROOT}/jar/OpenRTM*`
+  FILE2=`ls ${RTM_JAVA_ROOT}/jar/commons-cli*`
+  FILE3=`ls ${RTM_JAVA_ROOT}/jar/jna-?.?.?.jar`
+  FILE4=`ls ${RTM_JAVA_ROOT}/jar/jna-platform-*.jar`
+  FILE5=`ls MapServer/jyaml-?.?.*`
+  CLASSPATH=.:$FILE1:$FILE2:$FILE3:$FILE4:$FILE5:${RTM_JAVA_ROOT}/bin:MapServer
+  echo ${CLASSPATH}
+}
+if test "x$RTM_JAVA_ROOT" = "x" ; then
+    echo "Environment variable RTM_JAVA_ROOT is not set."
+    echo "Please specify the OpenRTM-aist installation directory."
+    echo "Abort."
+    exit 1
+fi
+export CLASSPATH=`get_classpath`
+java MapServerComp -f rtc.conf $*
---
+++ Navigation/NavigationManager.bat
@@ -0,0 +1,12 @@
+@set PATH=%RTM_ROOT%jre\bin;%PATH%
+@call set rtm_java_root=%RTM_JAVA_ROOT%
+@set JAR_BASE=%rtm_java_root%\jar\
+@for /F %%A in ('dir "%JAR_BASE%OpenRTM*" /B') do (set FILE1=%%A)
+@for /F %%A in ('dir "%JAR_BASE%commons-cli*" /B') do (set FILE2=%%A)
+@for /F %%A in ('dir "%JAR_BASE%jna-?.?.?.*" /B') do (set FILE3=%%A)
+@for /F %%A in ('dir "%JAR_BASE%jna-platform-?.?.?.*" /B') do (set FILE4=%%A)
+@set CLASSPATH=.;%JAR_BASE%%FILE1%;%JAR_BASE%%FILE2%;%JAR_BASE%%FILE3%;%JAR_BASE%%FILE4%;NavigationManager
+
+
+java NavigationManagerComp -f rtc.conf %*
+pause;
